21409c8cd39ed327b2f6ddbf026668f69d5ac937
Michael Blumenthal <bloomer25@googlemail.com>
2018-10-28 23:18:20 +0000

    destructures family in instrumentfamilyview

--- client/src/views/instrument_family_view.js
+++ client/src/views/instrument_family_view.js
@@ -13,19 +13,19 @@ class InstrumentFamilyView {
     });
   }
 
-  render(family){
+  render({name, description, instruments} = {}){
     this.container.innerHTML = '';
 
-    const familyName = this.createElement('h2', family.name);
+    const familyName = this.createElement('h2', name);
     this.container.appendChild(familyName);
 
-    const familyDescription = this.createElement('p', family.description);
+    const familyDescription = this.createElement('p', description);
     this.container.appendChild(familyDescription);
 
     const instrumentListTitle = this.createElement('h3', 'Instruments include:');
     this.container.appendChild(instrumentListTitle);
 
-    const instrumentList = this.createInstrumentList(family.instruments);
+    const instrumentList = this.createInstrumentList(instruments);
     this.container.appendChild(instrumentList);
   };
 
